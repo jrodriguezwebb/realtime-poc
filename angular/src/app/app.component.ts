3c406db692179bb77686f76d71f832365c36430e
Rodriguez <764092@cognizant.com>
2019-05-09 17:43:47 +0200

    adding ws logic to BE and FE

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    this.testWS();
+  }
+
+  testWS() {
+    const subject = webSocket('ws://localhost:3000');
+
+    subject.subscribe(
+      events => console.log('message received: ' + events), // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+  }
 }
